b3ec538c1136eeb8e9190901fbba277aa8ae188d
Can Bülbül <can@orkestra.co>
2021-08-17 00:48:08 +0300

    production ready

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
-import Lib
+import Server
+import Data.Proxy
+import Network.Wai
+import Network.Wai.Handler.Warp
+--import Network.Wai.Handler.WarpTLS
+import Servant
+
+api :: Proxy API
+api = Proxy
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "running server"
+    run 8080 $ serve api server
